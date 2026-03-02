6909c866ac3f2117c63959e9e5ed914c4cf5ac4a
maneesh-halcyontek <151718217+maneesh-halcyontek@users.noreply.github.com>
2026-03-02 11:48:08 +0530

    Heading change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import {Link } from "react-router-dom";
 
 const Home = ({ tasks }) => (
   <div>
-    <h1>My Tasks</h1>
+    <h1>My Tasks List</h1>
     <Link to="/add">Add New Task</Link>
     <ul>
       {tasks.map((t, i) => (
